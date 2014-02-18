26ac1f97d41323d81b007e61ae2c9fcef35cad7e
Rangken <rangken@gmail.com>
2014-02-18 09:32:34 +0900

    :sunglasses:

--- beautiful.sh
+++ beautiful.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --left-right "
-
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --left-right"
